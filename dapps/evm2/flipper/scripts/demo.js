e8992e4ca042ae7ae2ae8c252a672fdcd836c978
Luke Schoen <ltfschoen@gmail.com>
2023-06-28 10:10:12 +1000

    able to get instance of vrf contract

--- dapps/evm2/flipper/scripts/demo.js
+++ dapps/evm2/flipper/scripts/demo.js
@@ -5,45 +5,47 @@ const ethers = require('ethers');
 // https://docs.alchemy.com/docs/interacting-with-a-smart-contract
 const VRFD20ContractBuilt = require("../build/contracts/VRFD20.json");
 
-// Provider
-// https://docs.ethers.org/v6/api/providers/#WebSocketProvider
-const alchemyProvider = new ethers.AlchemyProvider(
-    chainId=11155111,
-    // url=process.env.CHAINLINK_SEPOLIA_ENDPOINT,
-    // network=Network.ETH_SEPOLIA,
-    // "eth-sepolia",
-    process.env.ALCHEMY_API_KEY
-);
-
-// Signer
-const signer = new ethers.Wallet(process.env.MOONBASE_PRIVATE_KEY, alchemyProvider);
-
-// Contract
-const VRFD20DeployedAtAddress = '0xe22cdfA9d8C8e942B498696ef54584426d2f5Dd6';
-const VRFD20Contract = new ethers.Contract(VRFD20DeployedAtAddress, VRFD20ContractBuilt.abi, signer);
-
 const config = {
     apiKey: process.env.ALCHEMY_API_KEY,
     network: Network.ETH_SEPOLIA,
 };
 
-const alchemy = new Alchemy(config);
-// console.log('alchemy', alchemy);
+const alchemyProvider = new Alchemy(config);
+console.log('alchemyProvider', alchemyProvider);
 
-// Listen to all new pending transactions
-alchemy.ws.on(
-    {
-        method: "alchemy_pendingTransactions",
-        fromAddress: "0x1dd907ABb024E17d196de0D7Fe8EB507b6cCaae7"
-    },
-    (res) => console.log('detected pending tx from account:', res),
-);
+// // Provider
+// // https://docs.ethers.org/v6/api/providers/#WebSocketProvider
+// const alchemyProvider = new ethers.AlchemyProvider(
+//     // _network: 11155111,
+//     // url=process.env.CHAINLINK_SEPOLIA_ENDPOINT,
+//     _network=Network.ETH_SEPOLIA,
+//     // "eth-sepolia",
+//     process.env.ALCHEMY_API_KEY
+// );
+
+// Signer
+const signer = new ethers.Wallet(process.env.MOONBASE_PRIVATE_KEY, alchemyProvider);
+console.log('signer', signer);
+
+// Contract
+const VRFD20DeployedAtAddress = '0xe22cdfA9d8C8e942B498696ef54584426d2f5Dd6';
+const VRFD20Contract = new ethers.Contract(VRFD20DeployedAtAddress, VRFD20ContractBuilt.abi, signer);
+console.log('VRFD20Contract', VRFD20Contract);
+
+// // Listen to all new pending transactions
+// alchemy.ws.on(
+//     {
+//         method: "alchemy_pendingTransactions",
+//         fromAddress: "0x1dd907ABb024E17d196de0D7Fe8EB507b6cCaae7"
+//     },
+//     (res) => console.log('detected pending tx from account:', res),
+// );
 
 // Example of using the call method
 const main = async () => {
-    // Get the latest block
-    const latestBlock = alchemy.core.getBlockNumber();
-    console.log('latestBlock', latestBlock);
+    // // Get the latest block
+    // const latestBlock = alchemy.core.getBlockNumber();
+    // console.log('latestBlock', latestBlock);
 
     // Interact with VRFD20
     const s_owner = await VRFD20Contract.s_owner();
