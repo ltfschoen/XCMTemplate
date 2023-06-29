5d3b2c3a55374fcc32800994a9798777966ae7f8
Luke Schoen <ltfschoen@gmail.com>
2023-06-29 10:14:11 +1000

    fix migration, requires min deposit of 1 eth to not error

--- dapps/evm2/flipper/migrations/2_deploy_contracts.js
+++ dapps/evm2/flipper/migrations/2_deploy_contracts.js
@@ -2,32 +2,37 @@ require('dotenv').config()
 const { Web3 } = require('web3');
 const BN = require('bn.js');
 
-var VRFD20 = artifacts.require("../build/contracts/VRFD20");
-// var RandomNumber = artifacts.require("../build/contracts/RandomNumber");
-// var Flipper = artifacts.require("../build/contracts/Flipper");
-
 console.log('deploying...');
 module.exports = async function (deployer) {
   let providerInstance;
   let web3;
-  // console.log('deploy_contracts to Moonbase Alpha');
-  
-  // providerInstance = new Web3.providers.WebsocketProvider(process.env.MOONBASE_BLASTAPI_ENDPOINT, {}, { delay: 500, autoReconnect: true, maxAttempts: 100 });
-  // console.log('providerInstance: ', providerInstance);
-  
-  // web3 = new Web3(providerInstance);
-  // console.log('web3.currentProvider: ', web3.currentProvider);
-  // deployer.deploy(RandomNumber, { value: web3.utils.toWei('0.000001', 'ether') });
-  // deployer.deploy(Flipper, false);
+  if (deployer.network == "moonbase") {
+    console.log('deploy_contracts to Moonbase Alpha');
 
-  console.log('deploy_contracts to Chainlink Sepolia');
-  providerInstance = new Web3.providers.WebsocketProvider(process.env.CHAINLINK_SEPOLIA_ENDPOINT, {}, { delay: 500, autoReconnect: true, maxAttempts: 100 });
-  console.log('providerInstance: ', providerInstance);
-  web3 = new Web3(providerInstance);
-  console.log('web3.currentProvider: ', web3.currentProvider);
-  const subscriptionId = 3217; // https://vrf.chain.link/
-  // wants 128620983229604640 wei
-  const value = web3.utils.toWei('0.000001', 'ether');
-  const amount = new BN(value, 10);
-  deployer.deploy(VRFD20, subscriptionId, { value: amount });
+    const RandomNumber = artifacts.require("../build/contracts/RandomNumber");
+    const Flipper = artifacts.require("../build/contracts/Flipper");
+  
+    providerInstance = new Web3.providers.WebsocketProvider(process.env.MOONBASE_BLASTAPI_ENDPOINT, {}, { delay: 500, autoReconnect: true, maxAttempts: 100 });
+    console.log('providerInstance: ', providerInstance);
+    
+    web3 = new Web3(providerInstance);
+    console.log('web3.currentProvider: ', web3.currentProvider);
+    // must be at least 1 ether since it must be greater than `REQUEST_DEPOSIT_AMOUNT`
+    deployer.deploy(RandomNumber, { value: web3.utils.toWei('1', 'ether') });
+    deployer.deploy(Flipper, false);
+  } else if (deployer.network == "sepolia") {
+    console.log('deploy_contracts to Chainlink Sepolia');
+
+    const VRFD20 = artifacts.require("../build/contracts/VRFD20");
+
+    providerInstance = new Web3.providers.WebsocketProvider(process.env.CHAINLINK_SEPOLIA_ENDPOINT, {}, { delay: 500, autoReconnect: true, maxAttempts: 100 });
+    console.log('providerInstance: ', providerInstance);
+    web3 = new Web3(providerInstance);
+    console.log('web3.currentProvider: ', web3.currentProvider);
+    const subscriptionId = 3217; // https://vrf.chain.link/
+    // wants 128620983229604640 wei
+    const value = web3.utils.toWei('0.000001', 'ether');
+    const amount = new BN(value, 10);
+    deployer.deploy(VRFD20, subscriptionId, { value: amount });
+  }
 };
