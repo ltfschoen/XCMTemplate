94b9d0a018298331e541e6daf6679880de79bf5b
Luke Schoen <ltfschoen@gmail.com>
2023-07-18 00:13:57 +1000

    update solidity version. simplify increment. uint256 instead of uint

--- dapps/evm2/flipper_game/contracts/FlipperGame.sol
+++ dapps/evm2/flipper_game/contracts/FlipperGame.sol
@@ -4,11 +4,11 @@ pragma solidity >=0.8.3;
 import {FlipperGameRandomNumber} from "./lib/FlipperGameRandomNumber.sol";
 
 contract FlipperGame {
-    uint private constant BLOCKS_ALLOW_GUESS = 100;
-    uint private constant BLOCKS_ALLOW_RANDOMNESS = 12; // 70 sec @ 6 sec/block
-    uint private constant BLOCKS_ALLOW_FULFILL = 7; // 40 sec @ 6 sec/block
+    uint256 private constant BLOCKS_ALLOW_GUESS = 100;
+    uint256 private constant BLOCKS_ALLOW_RANDOMNESS = 12; // 70 sec @ 6 sec/block
+    uint256 private constant BLOCKS_ALLOW_FULFILL = 7; // 40 sec @ 6 sec/block
 
-    uint public blockNumber;
+    uint256 public blockNumber;
     // bytes32 public blockHashNow;
     bytes32 public blockHashPrevious;
     bool public hasCalledFallbackFn;
@@ -23,14 +23,14 @@ contract FlipperGame {
         uint256 id;
         bytes32 startGameAtBlockHash; // blockHashPrevious
         address createdBy;
-        uint createdAtBlock;
-        uint endGuessesAtBlock;
+        uint256 createdAtBlock;
+        uint256 endGuessesAtBlock;
     }
 
     struct PlayerStruct {
         address playerAddress;
         uint256 gameId;
-        uint createdAtBlock;
+        uint256 createdAtBlock;
     }
 
     struct PlayerGuessStruct {
@@ -38,14 +38,7 @@ contract FlipperGame {
         uint256 gameId;
         address guessByPlayerAddress;
         uint256 guess;
-        uint createdAtBlock;
-    }
-
-    struct AnswerStruct {
-        uint256 id;
-        uint256 gameId;
-        uint256 answer;
-        uint createdAtBlock;
+        uint256 createdAtBlock;
     }
 
     struct RequestBlockStruct {
@@ -53,20 +46,27 @@ contract FlipperGame {
         uint256 requestedFulfillAtBlock;
     }
 
-    mapping(address => uint) public playerBalance;
+    struct AnswerStruct {
+        uint256 id;
+        uint256 gameId;
+        uint256 answer;
+        uint256 createdAtBlock;
+    }
+
+    mapping(address => uint256) public playerBalance;
     mapping(uint256 => GameStruct) public gameForGameId;
     mapping(uint256 => PlayerStruct[]) public playersForGameId;
     mapping(address => uint256[]) public gameIdsForPlayerId;
-    mapping(uint256 => PlayerGuessStruct[]) public playerGuessForPlayerOfGameId;
+    mapping(uint256 => PlayerGuessStruct[]) public playerGuessesForPlayerOfGameId;
     mapping(uint256 => RequestBlockStruct) public requestedAtBlockNumberForGameId;
     mapping(uint256 => AnswerStruct) public answerForGameId;
     event CreatedGame(uint256 indexed gameId, bytes32 blockHashPrevious, address indexed createdBy,
-        uint indexed createdAtBlock, uint endGuessesAtBlock);
-    event AddedPlayerToGame(uint256 indexed gameId, address indexed playerAddress, uint indexed createdAtBlock);
+        uint256 indexed createdAtBlock, uint256 endGuessesAtBlock);
+    event AddedPlayerToGame(uint256 indexed gameId, address indexed playerAddress, uint256 indexed createdAtBlock);
     event AddedGuessForPlayerOfGame(uint256 indexed gameId, uint256 indexed playerGuessId,
-        address indexed guessByPlayerAddress, uint256 guess, uint createdAtBlock);
+        address indexed guessByPlayerAddress, uint256 guess, uint256 createdAtBlock);
     event AddedAnswerForGame(uint256 indexed gameId, uint256 indexed answerId, address answerByAddress,
-        uint256 answer, uint indexed createdAtBlock);
+        uint256 answer, uint256 indexed createdAtBlock);
 
     constructor() {
         s_owner = msg.sender;
@@ -83,25 +83,24 @@ contract FlipperGame {
     //
     // credit: @KevinMoonbeam
     function createGame(uint256 _initialGuess) external payable {
-        require(_initialGuess <= 20);
+        require(_initialGuess <= 20, "initial guess too high");
         blockNumber = block.number;
         blockHashPrevious = blockhash(blockNumber - 1);
 
-        gameId = gameId + 1;
-        uint256 _gameId = gameId;
+        gameId += 1;
 
         GameStruct memory gameInstance = GameStruct({
-            id: _gameId,
+            id: gameId,
             startGameAtBlockHash: blockHashPrevious,
             createdBy: msg.sender,
             createdAtBlock: blockNumber,
             endGuessesAtBlock: blockNumber + BLOCKS_ALLOW_GUESS
         });
 
-        gameForGameId[_gameId] = gameInstance;
+        gameForGameId[gameId] = gameInstance;
         gamesList.push(gameInstance);    
 
-        emit CreatedGame(_gameId, blockHashPrevious, msg.sender, blockNumber, blockNumber + BLOCKS_ALLOW_GUESS);
+        emit CreatedGame(gameId, blockHashPrevious, msg.sender, blockNumber, blockNumber + BLOCKS_ALLOW_GUESS);
     }
 
     function addPlayerToGame(uint256 _gameId, uint256 _initialGuess) public payable {
@@ -124,7 +123,7 @@ contract FlipperGame {
     }
 
     function hasPlayerForGameId(uint256 _gameId) public view returns (bool) {
-        for (uint i = 0; i < playersForGameId[_gameId].length; i++) {
+        for (uint256 i = 0; i < playersForGameId[_gameId].length; i++) {
             if (playersForGameId[_gameId][i].playerAddress == msg.sender) {
                 return true;
             }
@@ -133,7 +132,7 @@ contract FlipperGame {
     }
 
     function hasPlayerGuessedForGameId(uint256 _gameId) public view returns (bool) {
-        for (uint i = 0; i < gameIdsForPlayerId[msg.sender].length; i++) {
+        for (uint256 i = 0; i < gameIdsForPlayerId[msg.sender].length; i++) {
             if (gameIdsForPlayerId[msg.sender][i] == _gameId) {
                 return true;
             }
@@ -148,19 +147,18 @@ contract FlipperGame {
         require(blockNumber >= gameForGameId[_gameId].createdAtBlock);
         require(blockNumber <= gameForGameId[_gameId].endGuessesAtBlock,
             "guesses only allowed before block number when guesses end");
-        playerGuessId = playerGuessId + 1;
-        uint256 _playerGuessId = playerGuessId;
+        playerGuessId += 1;
         PlayerGuessStruct memory playerGuessInstance = PlayerGuessStruct({
-            id: _playerGuessId,
+            id: playerGuessId,
             gameId: _gameId,
             guessByPlayerAddress: msg.sender,
             guess: _guess,
             createdAtBlock: blockNumber
         });
-        playerGuessForPlayerOfGameId[_gameId].push(playerGuessInstance);
+        playerGuessesForPlayerOfGameId[_gameId].push(playerGuessInstance);
         gameIdsForPlayerId[msg.sender].push(_gameId);
 
-        emit AddedGuessForPlayerOfGame(_gameId, _playerGuessId, msg.sender, _guess, blockNumber);
+        emit AddedGuessForPlayerOfGame(_gameId, playerGuessId, msg.sender, _guess, blockNumber);
 
         return _guess;
     }
